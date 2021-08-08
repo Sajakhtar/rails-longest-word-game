3744e493cdafe31724aad3e77fc6dc5bb6359dba
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-08 17:43:26 +0400

    update application test case file with headless chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,12 +4,16 @@ require "test_helper"
 #   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 # end
 
+# class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+#   Capybara.register_driver(:headless_chrome) do |app|
+#     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome \
+#       chromeOptions: { args: %w[headless disable-gpu window-size=1280x760] }
+#     Capybara::Selenium::Driver.new app,
+#       browser: :chrome, desired_capabilities: capabilities
+#   end
+#   driven_by :headless_chrome
+# end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  Capybara.register_driver(:headless_chrome) do |app|
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome \
-      chromeOptions: { args: %w[headless disable-gpu window-size=1280x760] }
-    Capybara::Selenium::Driver.new app,
-      browser: :chrome, desired_capabilities: capabilities
-  end
-  driven_by :headless_chrome
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end
