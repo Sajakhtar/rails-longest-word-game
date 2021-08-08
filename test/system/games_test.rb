4dd35c9bbaeab51ab242bd3ebbaff57003153e77
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-08 16:47:08 +0400

    add capybara tests

---
+++ test/system/games_test.rb
@@ -0,0 +1,31 @@
+require "application_system_test_case"
+
+class GamesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit games_url
+  #
+  #   assert_selector "h1", text: "Game"
+  # end
+
+  test "Going to /new gives us a new random grid to play with" do
+    visit new_url
+    assert test: "New game"
+    assert_selector "li", count: 10
+  end
+
+  test "fill in non-english word to go to /score" do
+    visit new_url
+    fill_in "attempt", with: "ddddd"
+    click_on "Play"
+
+    assert_text "ddddd is not an English word!"
+  end
+
+  test "fill in english word to go to /score" do
+    visit new_url
+    fill_in "attempt", with: "dad"
+    click_on "Play"
+
+    assert_text "dad iis not from the grid of letters:"
+  end
+end
