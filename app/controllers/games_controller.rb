4cf4845b47b59767848756bfd42b5fa8c35af4fe
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-08 16:24:22 +0400

    add total score via session cookie

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,6 +12,7 @@ class GamesController < ApplicationController
     @grid = params[:grid].split('')
 
     @result = get_result(@attempt, @grid)
+    # binding.pry
   end
 end
 
@@ -26,12 +27,17 @@ def get_score(word, grid)
   (word.size.to_f / grid.size * 100)
 end
 
+def get_total(points)
+  session[:score] ? session[:score] += points : session[:score] = 0
+end
+
 def get_result(word, grid)
   if english_word?(word)
     condition = word.upcase.chars.all? { |char| grid.count(char) >= word.upcase.chars.count(char) }
-    return { score: get_score(word, grid), message: 'is a valid word. Well Done!' } if condition
+    points = get_score(word, grid)
+    return { score: points, total: get_total(points), message: 'is a valid word. Well Done!' } if condition
 
-    return { score: 0, message: "does not contain characters from the grid of letters: #{grid.join(', ')}!" }
+    return { score: 0, total: get_total(0), message: "is not from the grid of letters: #{grid.join(', ')}!" }
   end
-  { score: 0, message: 'is not an English word!' }
+  { score: 0, total: get_total(0), message: 'is not an English word!' }
 end
